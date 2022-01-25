357f25102060c9878bb910254007fd668f98f5fd
heshanu <heshanu97@gmail.com>
2022-01-25 22:54:43 +0530

    2 files modified-added custom pipe with different attributes!

--- src/app/convert.pipe.ts
+++ src/app/convert.pipe.ts
@@ -5,9 +5,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ConvertPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    if(!value){ return ''}
-    return value*1.6;
+  transform(value: any,targetUnits:string): any {
+    if(!value){ return '' }
+      switch(targetUnits){
+        case 'km':
+          return value*1.6;break;
+        case 'm':
+          return value*1.6*1000;break;
+        case 'cm':
+          return value*1.6*1000*1000;break;
+        default:
+          throw new Error('Target unit isnot matches!');
+      }
+      return value*1.6;
   }
 
 }
