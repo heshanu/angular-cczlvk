1504fc5f7f41f2896f4280a2f2ffcf3bedb7adb3
heshanu <heshanu97@gmail.com>
2022-01-25 22:31:36 +0530

    2 files modified-solve convert pipe and convert miles to killometers

--- src/app/convert.pipe.ts
+++ src/app/convert.pipe.ts
@@ -6,7 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ConvertPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    return "helo";
+    if(!value){ return ''}
+    return value*1.6;
   }
 
 }
+
+//custom pipelines first we add path to app.module.ts then
+//second add to pipe name to declarable  
