8d5188050dfb133f020df7dadb886408fb89166b
heshanu <heshanu97@gmail.com>
2022-01-25 22:25:20 +0530

    4 files modified addde pipe and import pipe to module ts and component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import {ConvertPipe} from './convert.pipe';
+
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent,ConvertPipe ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
